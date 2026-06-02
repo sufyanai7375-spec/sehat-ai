b1c5e5e832800a68552341b89dd0862100655c55
Sufyan Arshad <sufyanai7375@gmail.com>
2026-06-03 04:47:22 +0500

    Update utils.ts

--- sehat-ai/src/lib/utils.ts
+++ sehat-ai/src/lib/utils.ts
@@ -46,7 +46,8 @@ export function formatMarkdown(text: string): string {
     .replace(/^# (.*$)/gim, "<h1>$1</h1>")
     .replace(/^\* (.*$)/gim, "<li>$1</li>")
     .replace(/^- (.*$)/gim, "<li>$1</li>")
-    .replace(/(<li>.*<\/li>)/gims, "<ul>$1</ul>")
+    // .replace(/(<li>.*<\/li>)/gims, "<ul>$1</ul>")
+    .replace(/(<li>[\s\S]*?<\/li>)/gim, "<ul>$1</ul>")
     .replace(/\n/g, "<br>");
 }
 
